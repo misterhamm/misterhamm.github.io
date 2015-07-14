c005382d34e4362829839fc859a3a935a3739826
Chris Hamm <chris.m.hamm@gmail.com>
2015-07-13 23:56:54 -0400

    styling refactored for smaller screen, jump links fixed, animations set

---
+++ js/app.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+
+
+/*$('.project-square').mouseenter(function(){
+    $(this).find('.project-wrapper').show();
+});
+
+$('.project-square').mouseleave(function() {
+    $(this).find('.project-wrapper').hide();
+});*/
+
+
+
+
+
+
+
+
+
+
+});
